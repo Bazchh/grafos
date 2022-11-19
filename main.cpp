e86103875e01add79c65fbac026738e4acdb8f34
Bazch <mikael.vidal@gmail.com>
2022-11-19 19:28:40 -0300

    capturando cidades e coordenadas

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include "bibliotecas/grafos.cpp"
 
 int main(){
-    dataItem *d;
+    dataItem *d = (dataItem*)malloc(sizeof(dataItem));
     grafo g;
+    char *arquivo1 =(char*) "cidades.txt";
+    char *arquivo2 =(char*) "coordenadasrn.csv";    
+    cidade *cidades =  getCidades(arquivo1);
+    gps *localizacao =  getGps(arquivo2);
+    d = getItens(cidades,localizacao);
     init_grafo(g);
     return 0;
 }
