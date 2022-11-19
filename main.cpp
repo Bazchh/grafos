7336cf7f48e0e3b071a92e9c0c1a016f9c278771
Bazch <mikael.vidal@gmail.com>
2022-11-19 20:27:23 -0300

    mudança de variaveis

--- main.cpp
+++ main.cpp
@@ -3,11 +3,12 @@
 int main(){
     dataItem *d = (dataItem*)malloc(sizeof(dataItem));
     grafo g;
-    char *arquivo1 =(char*) "cidades.txt";
-    char *arquivo2 =(char*) "coordenadasrn.csv";    
-    cidade *cidades =  getCidades(arquivo1);
-    gps *localizacao =  getGps(arquivo2);
-    d = getItens(cidades,localizacao);
     init_grafo(g);
+    char *arquivo1 =(char*) "cidades.txt";
+    char *arquivo2 =(char*) "coordenadas.csv";    
+    cidade *cidades = getCidades(arquivo1);
+    gps *local = getGps(arquivo2);
+    d = getItens(cidades,local);
+    
     return 0;
 }
