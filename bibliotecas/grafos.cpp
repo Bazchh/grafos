db0763ab9a2a9d67639ee5419ccaef549720b917
Bazch <mikael.vidal@gmail.com>
2022-11-19 22:49:17 -0300

    atualização de funções

--- bibliotecas/grafos.cpp
+++ bibliotecas/grafos.cpp
@@ -1,16 +1,26 @@
 #ifndef GRAFOS_CPP
 #define GRAFOS_CPP
-#define SIZEG 160
+#define SIZEG 161
 #include "hash.h"
 typedef dataItem **grafo[SIZEG][SIZEG];
+
 void init_grafo(grafo G){
-    int i;
-    int j;
+    int i, j;
+
     for(i = 0; i < SIZEG; i++){
         for(j = 0; j < SIZEG; j++){
             G[i][j] = 0;
-        }
+        } 
     }
+}
+
+int inserir_dados(dataItem *d, grafo g){
+    int i, j;
+    dataItem aux;
+    for(i = 0; i < SIZEG; i++){
+       
+    }
+    return 0;
 }
 
 #endif
