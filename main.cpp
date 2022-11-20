10f1f83023afab7d7dc0e29c6db87bfccd301bcc
Bazch <mikael.vidal@gmail.com>
2022-11-19 22:50:06 -0300

    alocação de matriz de DataItens (struct)

--- main.cpp
+++ main.cpp
@@ -4,11 +4,17 @@ int main(){
     dataItem *d = (dataItem*)malloc(sizeof(dataItem));
     grafo g;
     init_grafo(g);
+    int i = 0, j = 0;
+    dataItem **m = (dataItem**)malloc(SIZEG*sizeof(dataItem*));
+    
+    for(i = 0; i < SIZEG; i++){
+        m[i] = (dataItem*)malloc(sizeof(dataItem));
+    }
+    
     char *arquivo1 =(char*) "cidades.txt";
     char *arquivo2 =(char*) "coordenadas.csv";    
     cidade *cidades = getCidades(arquivo1);
     gps *local = getGps(arquivo2);
-    d = getItens(cidades,local);
-    
+    d = getItens(cidades,local);   
     return 0;
 }
