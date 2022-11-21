023ea10f2b881bde2414f4bfc2ff6fb0f6de2d80
Bazch <mikael.vidal@gmail.com>
2022-11-21 13:09:42 -0300

    testes +1

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@ int main(){
     int i = 0, j = 0;
     dataItem **m;
     m = aloca_grafo(m);
-    char *arquivo1 =(char*) "cidades.txt";
-    char *arquivo2 =(char*) "coordenadas.csv";    
+    char *arquivo1 =(char*) "cidades.csv";
+    char *arquivo2 =(char*) "coordenada.csv";    
     cidade *cidades = getCidades(arquivo1);
-    gps *local = getGps(arquivo2);
+    gps *local = getGps(arquivo2,cidades);
     d = getItens(cidades,local);
     inserir_dados(d,m); 
      m = quilometros(m);
