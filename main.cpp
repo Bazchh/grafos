24e0a7bc927e79a812c6eef3feb0b2a4f70ab948
Bazch <mikael.vidal@gmail.com>
2022-11-22 10:49:09 -0300

    updates finais

--- main.cpp
+++ main.cpp
@@ -5,13 +5,23 @@ int main(){
     //dataItem **m;
     //m = aloca_grafo(m);
     dataItem *G = cria_grafo((char*) "cidades.csv",(char*) "coordenada.csv");
-    float D[] = {0.05,0.1,0.15,0.20,0.25}, V;
+    float D[] = {0.05,0.1,0.15,0.20,0.25};
 
     vizinhanca R;
-
-    R = matriz_grafo(G,D[0]);
-
-    printf("\nCidade com mais vizinhos: %i na posição %i\n\n", R.qntvizinhos, R.pos);
+    for(int i = 0; i < 5; i++){
+    R = matriz_grafo(G,D[i]);
+    printf("\nA cidade com mais vizinhos com base na distancia minima D = %f está"
+    "\nna posição [%i] %s com %i vizinhos\n",D[i], R.pos,G[R.pos].city.cidade, R.qntvizinhos);
+    if(R.pos == R.posvoid){
+        printf("\nNão existe cidade sem vizinhos com base na distancia minima %f\n", D[i]);
+        printf("\n==========================================================================\n\n");
+    }else if(R.pos != R.posvoid){
+        printf("\nA cidade que não possui vizinhos com base na distancia minima %f está"
+    "\nna posição [%i] %s", D[i], R.posvoid, G[R.posvoid].city.cidade);
+    printf("\n\n============================================================================\n\n");
+    }
+    
+    }
     //inserir_dados(d,m); 
      //m = quilometros(m);
     return 0;
